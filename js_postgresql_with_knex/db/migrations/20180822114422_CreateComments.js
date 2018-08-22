acc95c5c6130c62e809c77ac263626c29db37ea2
Steve Godin <steve.god@gmail.com>
2018-08-22 12:06:44 -0700

    Seed comments and fix column names in comments table

--- js_postgresql_with_knex/db/migrations/20180822114422_CreateComments.js
+++ js_postgresql_with_knex/db/migrations/20180822114422_CreateComments.js
@@ -2,8 +2,8 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("comments", t => {
     t.increments("id");
     t.text("content");
-    t.integer("post_id");
-    t.timestamp("created_at").defaultTo(knex.fn.now());
+    t.integer("postId");
+    t.timestamp("createdAt").defaultTo(knex.fn.now());
   });
 };
 
