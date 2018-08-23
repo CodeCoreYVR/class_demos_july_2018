c6982c0d02f0d1616230f03027d57a6f98543412
Steve Godin <steve.god@gmail.com>
2018-08-23 10:15:01 -0700

    Split all routes from app.js into own route modules

--- js_express_and_rest/app.js
+++ js_express_and_rest/app.js
@@ -100,146 +100,16 @@ app.use((request, response, next) => {
 // R O U T E S
 // -----------
 
-// The following method creates a "route" with a callback that
-// used to build the response that'll be sent back to the client.
-// In the code below, HTTP requests made to the path "/hello_world"
-// are handled by this callback which sends back the text
-// "Hello, World!"
+const welcomeRouter = require("./routes/welcome");
+const postsRouter = require("./routes/posts");
 
-// The first argument to the method "get" is the URL path and the
-// second is a callback.
-app.get("/hello_world", (request, response) => {
-  // The "request" argument is an object that represents the request
-  // being made by the client. It contains information about the client
-  // and everything the client is asking for.
-
-  // The "response" argument is an object that represents the server's
-  // reply to the client. We build out the content to send back to
-  // the client with it.
-  response.send("Hello, Peoples!");
-});
-
-app.get("/", (request, response) => {
-  // `response.render(<ejs-filepath>)` is used
-  // to render a template from the "views/" directory at
-  // the relative file path of <ejs-filepath>. When writing
-  // the file path, you can ignore the extension.
-
-  // In the call below, the file at "./views/welcome.ejs" is
-  // rendered into HTML and is sent as the content of the response
-  // to the client.
-  response.render("welcome");
-});
-
-app.get("/survey", (request, response) => {
-  console.log("---------------------------");
-  console.log("Request's Query String Data");
-  console.log("---------------------------");
-  console.log(request.query);
-
-  // "request.query" is a property that holds an object
-  // which contains of all the key-value pairs from
-  // a URL's query string.
-  // For example:
-  // .../survey?fullName=Steve&color=%2300fdff
-  // request.query === { fullName: "Steve", color: "#00fdff"} // true
-
-  response.render("survey");
-});
-
-app.get("/survey/results", (request, response) => {
-  // response.send(request.query);
-  const fullName = request.query.fullName;
-  const color = request.query.color;
-  const timeOfDay = request.query.timeOfDay;
-
-  // (Optional) `response.render` can take an object as
-  // a second argument where all its key-value pairs will
-  // turned into variables inside of the template that is rendered.
-  // This is how we share data with template files.
-  response.render("surveyResults", {
-    fullName: fullName,
-    color: color,
-    timeOfDay: timeOfDay
-  });
-});
-
-// URL http://localhost:4545/survey?fullName=Steve&color=%2300fdff
-//   scheme | host     |port| path | query string (search string)
-
-// A "query" is a way to store data in the data in the URL.
-// It use the URL Encoding format.
-
-// Following a url's path and seperated by a "?" will be a
-// a query string which holds key-value pairs of data.
-// The format appears as follows:
-// ?[key_1]=[value_1]&[key_2]=[value_2]&[key_3]=[value_3]
-
-// When a form is submitted, its data will by default be included
-// into the URL with the above format.
-
-const COOKIE_MAX_AGE = 1000 * 60 * 60 * 24 * 7;
-app.post("/sign_in", (request, response) => {
-  const username = request.body.username;
-
-  // `response.cookie(<cookie-name>, <cookie-value>, <options)`
-  // The above method is added to the `response` object by
-  // the `cookie-parser` middleware. Use to send cookies to
-  // the browser.
-  // - The first argument is a string that's the name of the cookie
-  // - The second, a value for the cookie
-  // - (optional) The last, options for the cookie
-
-  response.cookie("username", username, { maxAge: COOKIE_MAX_AGE });
-
-  // Like `response.send` and `response.render`, `response.redirect`
-  // ends the response with a redirect status code and a location (i.e. URL) where
-  // the browser should make a GET request to (should browse to).
-  response.redirect("/");
-});
-
-app.post("/sign_out", (request, response /*, next */) => {
-  response.clearCookie("username");
-  response.redirect("/");
-});
-
-// POSTS
-const knex = require("./db/client");
-
-// posts#new -> GET /posts/new
-app.get("/posts/new", (req, res) => {
-  res.render("posts/new");
-});
-
-// posts#create -> POST /posts
-app.post("/posts", (req, res) => {
-  // const imageUrl = req.body.imageUrl;
-  // const title = req.body.title;
-  // const content = req.body.content;
-  // 👇 syntax sugar for 👆
-  // Object destructuring
-  const { imageUrl, title, content } = req.body;
-
-  knex("posts")
-    // .insert({
-    //   imageUrl: imageUrl,
-    //   title: title,
-    //   content: content
-    // })
-    // 👇 syntax sugar for 👆
-    // When the name of key is the name as the variable
-    // that assigned to that key, you can use object
-    // short-hand property syntax.
-    .insert({
-      imageUrl,
-      title,
-      content
-    })
-    .returning("id")
-    .then(post => {
-      res.send(post);
-    });
-});
+app.use("/", welcomeRouter);
+// You can split routes into their modules with
+// express.Router() instance. When doing so, require the module
+// and connect to the app with app.use(<path-prefix>, <router-instance>).
+// The app.use(...) will route all routes inside of postsRouter
+// prefix all of their URLs with /posts.
+app.use("/posts", postsRouter);
 
 // ------------------
 // R U N  S E R V E R
---
+++ js_express_and_rest/routes/posts.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+const knex = require("../db/client");
+
+// posts#new -> GET /posts/new
+router.get("/new", (req, res) => {
+  res.render("posts/new");
+});
+
+// posts#create -> POST /posts
+router.post("/", (req, res) => {
+  // const imageUrl = req.body.imageUrl;
+  // const title = req.body.title;
+  // const content = req.body.content;
+  // 👇 syntax sugar for 👆
+  // Object destructuring
+  const { imageUrl, title, content } = req.body;
+
+  knex("posts")
+    // .insert({
+    //   imageUrl: imageUrl,
+    //   title: title,
+    //   content: content
+    // })
+    // 👇 syntax sugar for 👆
+    // When the name of key is the name as the variable
+    // that assigned to that key, you can use object
+    // short-hand property syntax.
+    .insert({
+      imageUrl,
+      title,
+      content
+    })
+    .returning("id")
+    .then(post => {
+      res.send(post);
+    });
+});
+
+module.exports = router;
